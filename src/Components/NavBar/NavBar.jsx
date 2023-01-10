9758c90b6d0eac280d4af767b5ba348e13fbdef4
facuferreria <ferreriafacundo@gmail.com>
2023-01-10 19:11:45 -0300

    Working Final Version

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,39 +1,24 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import brand from '../../assets/brand-logo.svg';
 import styles from './NavBar.module.scss'
 
 function NavBar() {
   return (
-    <Navbar key='xl' expand='xl' className={styles.nav}>
-        <Container fluid>
-            <Navbar.Brand href="#home"><img src={brand} alt= "brand logo" className={styles.logo} /></Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xl`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-xl`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-xl`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xl`}>
-                    <img src={brand} alt= "brand logo" className={styles.logo}  />
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className={`${styles.nav_options} flex-grow-1 pe-3 `}>
-                  <Nav.Link className={styles.nav_link} href="/#home">INICIO</Nav.Link>
-                  <Nav.Link className={styles.nav_link} href="/#about">SOBRE MI</Nav.Link>
-                  <Nav.Link className={styles.nav_link} href="/#proyects">PROYECTOS</Nav.Link>
-                  <Nav.Link className={styles.nav_link} href="/#contact">CONTACTO</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-        </Container>
+    <Navbar collapseOnSelect expand="lg" className={styles.nav}>
+      <Container fluid>
+        <Navbar.Brand href="#home"><img src={brand} alt= "brand logo" className={styles.logo} /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className={`${styles.nav_options} me-auto`}>
+            <Nav.Link className={styles.nav_link} href="#home">INICIO</Nav.Link>
+            <Nav.Link className={styles.nav_link} href="/#about">SOBRE MI</Nav.Link>
+            <Nav.Link className={styles.nav_link} href="/#proyects">PROYECTOS</Nav.Link>
+            <Nav.Link className={styles.nav_link} href="/#contact">CONTACTO</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -7,33 +7,31 @@ function Presentation() {
     return (
         <div className="container" id="home">
             <div className={styles.subcontainer}>
-                <div>
-                    <p className={styles.text} >Hola! Me llamo <span className={styles.blue_text}>Facundo.</span></p>
-                    <p className={styles.text} >Soy un frontend developer y mi objetivo principal es que juntos:</p>
-                    <Typewriter
-                        options={{ loop: true }}
-                        onInit={(typewriter) => {
-                            typewriter
-                            .pauseFor(1500)
-                            .changeDelay(40)
-                            .changeDeleteSpeed(30)
-                            .typeString('<strong>Desarrollemos esa pagina ideal que imaginas</strong>')
-                            .pauseFor(1500)
-                            .deleteChars(43)
-                            .typeString('<strong>Impulsemos ese negocio que tenes</strong>')
-                            .pauseFor(1500)
-                            .deleteChars(32)
-                            .typeString('<strong>Cumplamos ese sueño que queres</strong>')
-                            .pauseFor(1500)
-                            .deleteChars(30)
-                            .start();
-                        }}
-                    />
-                    <button className={styles.button} >
-                        Ve mis proyectos
-                        <FontAwesomeIcon className={styles.icon} icon="fa-solid fa-arrow-right" />
-                    </button>
-                </div>
+                <p className={styles.text} >Hola! Me llamo <span className={styles.blue_text}>Facundo.</span></p>
+                <p className={styles.text} >Soy un frontend developer y mi objetivo principal es que juntos:</p>
+                <Typewriter
+                    options={{ loop: true }}
+                    onInit={(typewriter) => {
+                        typewriter
+                        .pauseFor(1500)
+                        .changeDelay(40)
+                        .changeDeleteSpeed(30)
+                        .typeString('<strong>Desarrollemos esa pagina ideal que deseas</strong>')
+                        .pauseFor(1500)
+                        .deleteChars(41)
+                        .typeString('<strong>Impulsemos ese negocio que tenes</strong>')
+                        .pauseFor(1500)
+                        .deleteChars(32)
+                        .typeString('<strong>Cumplamos ese sueño que queres</strong>')
+                        .pauseFor(1500)
+                        .deleteChars(30)
+                        .start();
+                    }}
+                />
+                <a href="/#proyects" className={styles.button}>     
+                    Ve mis proyectos
+                    <FontAwesomeIcon className={styles.icon} icon="fa-solid fa-arrow-right" />                       
+                </a>
             </div>
         </div>
     )
